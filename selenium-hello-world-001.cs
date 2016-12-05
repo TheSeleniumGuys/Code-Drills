e9b417b50db6f15d7f4331c26c5e0431d08ba5f1
The Selenium Guys <info@theseleniumguys.com>
2016-12-05 01:46:14 +0000

    Create selenium-hello-world-001.cs
    One example of how to do a basic Selenium test using C#.

---
+++ selenium-hello-world-001.cs
@@ -0,0 +1,14 @@
+//See the blog post at: 
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpendQA.Selenium.IE;
+
+IWebdriver.driver = new InternetExplorerDriver();
+
+driver.Navigate()GoToURL("http://www.google.com");
+driver.FindElement(By.XPath("//input[@value='Google Search'])).SendKeys("Hello, World!");
+driver.FindElement(By.XPath("//input[@value='Google Search']")).Click();
+
+driver.Quit();
