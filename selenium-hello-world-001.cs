20fb986918ae71c208f583f751090ecbe0794ff8
The Selenium Guys <info@theseleniumguys.com>
2016-12-05 13:56:38 +0000

    Fixing minor syntax error

--- selenium-hello-world-001.cs
+++ selenium-hello-world-001.cs
@@ -8,7 +8,7 @@ using OpendQA.Selenium.IE;
 IWebdriver.driver = new InternetExplorerDriver();
 
 driver.Navigate().GoToURL("http://www.google.com");
-driver.FindElement(By.XPath("//input[@value='Google Search'])).SendKeys("Hello, World!");
+driver.FindElement(By.XPath("//input[@value='Google Search']")).SendKeys("Hello, World!");
 driver.FindElement(By.XPath("//input[@value='Google Search']")).Click();
 
 driver.Quit();
